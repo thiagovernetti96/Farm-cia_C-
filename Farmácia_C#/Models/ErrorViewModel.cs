12b74eb17d5a96d66548e5a443e677961dff160d
Thiago <thiagovernetti@hotmail.com>
2024-06-25 11:07:52 -0300

    Adicionar arquivos de projeto.

---
+++ Farmácia_C#/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Farmácia_C_.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
